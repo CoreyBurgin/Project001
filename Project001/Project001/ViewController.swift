ce671ee33d27e3e09e6b1d00b0cf7ad470726e08
CoreyBurgin <corey.burgin22@gmail.com>
2020-09-07 18:12:41 -0400

    Project was created
    no changes where made , project was just created

---
+++ Project001/Project001/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project001
+//
+//  Created by Corey Burgin on 9/7/20.
+//  Copyright © 2020 Corey Burgin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
